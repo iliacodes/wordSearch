b015ed62f572449cc8160e487729d8a3f2cc1538
iliacodes <6ac177ed@protonmail.com>
2022-11-23 18:27:06 -0800

    all files for pair-programming 11/23 updated

---
+++ test/test_wordsearch.js
@@ -0,0 +1,90 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const wordSearch = require('../wordsearch.js');
+
+describe("#wordSearch()", function() {
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
+
+    assert.isFalse(result); //return false instead of undefined
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['H', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['L', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['O', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HELLO');
+
+    assert.isTrue(result);
+  });
+
+  it("should return error if passed an empty array", function() {
+    const result = wordSearch([], 'HELLO');
+    assert.equal(result, "Error");
+  });
+
+  it("should return error if passed anything other than a string", function() {
+    const result = wordSearch([
+      ['H', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['L', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['O', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],], 123);
+    assert.equal(result, "Error");
+  });
+
+//  it("should return true if a string is spelt backwords", function() {
+    // const result = wordSearch([
+    //   ['o', 'O', 'L', 'L', 'E', 'H', 'A', 'L'],
+    //   ['e', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    //   ['l', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    //   ['l', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    //   ['h', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    //   ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    //   ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    //   ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    //   ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],], 123);
+    // assert.equal(result, "HELLO");
+//  });
+
+
+});
+
